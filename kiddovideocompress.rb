fae05dd288b0a69a89c2b0deca943266bcf85392
Leo <leonardo_cavalcante@msn.com>
2018-05-19 03:01:15 -0300

    Initil commit =)

---
+++ kiddovideocompress.rb
@@ -0,0 +1,18 @@
+
+class Kiddovideocompress < Formula
+  desc "Batch video compress with ffmpeg "
+  homepage "https://github.com/CavalcanteLeo/KiddoVideoCompress"
+  url "https://github.com/CavalcanteLeo/KiddoVideoCompress/archive/v0.0.1.tar.gz"
+  sha256 "3488c76602365220085d06334781070c25dbb71246fe0c51a9d0b427e46666e2"
+
+  depends_on "bash"
+
+  def install
+    bin.install => "bin/video_compress.sh"
+  end
+
+  test do
+
+    system "false"
+  end
+end
