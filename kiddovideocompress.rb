37b79f4322185c119b7757a347c707dc9d0b5fe2
Leo <leonardo_cavalcante@msn.com>
2018-05-19 04:41:01 -0300

    bump to version 1.0.0

--- kiddovideocompress.rb
+++ kiddovideocompress.rb
@@ -5,7 +5,7 @@ class Kiddovideocompress < Formula
   homepage "https://github.com/CavalcanteLeo/KiddoVideoCompress"
   url "https://github.com/CavalcanteLeo/KiddoVideoCompress/archive/v.1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "3488c76602365220085d06334781070c25dbb71246fe0c51a9d0b427e46666e2"
+  sha256 "28f51832c8f0a5d9ed663cfeb7b5bce9727b6ec5644538f7dde2f1982e763354"
 
   depends_on "bash"
 
