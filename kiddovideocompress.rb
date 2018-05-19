321588b0b41143371cf231f0a1fb69fb72dd6ae9
Leo <leonardo_cavalcante@msn.com>
2018-05-19 04:39:47 -0300

    bump to version 1.0.0

--- kiddovideocompress.rb
+++ kiddovideocompress.rb
@@ -3,7 +3,7 @@ class Kiddovideocompress < Formula
 
   desc "Batch video compress with ffmpeg"
   homepage "https://github.com/CavalcanteLeo/KiddoVideoCompress"
-  url "https://github.com/CavalcanteLeo/KiddoVideoCompress/archive/v1.0.0.tar.gz"
+  url "https://github.com/CavalcanteLeo/KiddoVideoCompress/archive/v.1.0.0.tar.gz"
   version "1.0.0"
   sha256 "3488c76602365220085d06334781070c25dbb71246fe0c51a9d0b427e46666e2"
 
